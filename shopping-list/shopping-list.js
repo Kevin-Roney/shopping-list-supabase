487d59571af864ff0bf23da440778e14fbaf8847
Kevin Roney <ktroney.pdx@gmail.com>
2022-03-29 10:59:25 -0700

    grabbed DOM from HTML, ready to start event listeners

--- shopping-list/shopping-list.js
+++ shopping-list/shopping-list.js
@@ -1,15 +1,18 @@
-import { checkAuth,
+import { 
+    checkAuth,
     logout,
     createItem,
     deleteAllItems,
     getItems,
     buyItem
 } from '../fetch-utils.js';
-
 import { renderItem } from '../render-utils.js';
 
 checkAuth();
 
+const itemEl = document.querySelector('.items');
+const itemForm = document.querySelector('.item-form');
+const deleteButton = document.querySelector('.delete-button');
 const logoutButton = document.getElementById('logout');
 
 logoutButton.addEventListener('click', () => {
